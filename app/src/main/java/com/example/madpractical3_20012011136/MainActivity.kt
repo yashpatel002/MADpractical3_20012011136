fa08f933ff25faf1a917a96c0c302d91444f1a48
Yash Patel <ympatel132635@gmail.com>
2022-09-11 23:24:35 +0530

    Done

--- app/src/main/java/com/example/madpractical3_20012011136/MainActivity.kt
+++ app/src/main/java/com/example/madpractical3_20012011136/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.madpractical3_20012011136
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import com.example.madpractical3_20012011136.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        binding.bottomNavigationView.setOnItemSelectedListener { it1 ->
+            when(it1.itemId)
+            {
+                R.id.bottom_nav_login->
+                {
+                    Intent(this,LoginActivity::class.java).also { startActivity(it) }
+                }
+                R.id.bottom_nav_reg->
+                {
+                    Intent(this,RegistrationActivity::class.java).also { startActivity(it) }
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 }
